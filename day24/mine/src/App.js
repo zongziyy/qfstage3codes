891560f750c116fa7978843bf83d12bce5e54117
zongziyy <1500422738@qq.com>
2021-03-15 22:06:47 +0800

    2021.3.15 22.06

--- day24/mine/src/App.js
+++ day24/mine/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import RouterView from "./router";
 
+
 export default class App extends Component {
-  render() {
-    return (
-      <RouterView></RouterView>
-    )
-  }
+    render() {
+        return (
+            <RouterView></RouterView>
+        )
+    }
 }
